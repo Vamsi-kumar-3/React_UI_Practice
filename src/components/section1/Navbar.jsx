25fa61d33d5c261fbc2c887c6530b3bcd21132cc
Vamsi-kumar-3 <vamsikumar@gmail.com>
2026-02-19 22:37:25 +0530

    Describe your changes

--- src/components/section1/Navbar.jsx
+++ src/components/section1/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div>
       <nav className="flex justify-between items-center p-6"> 
         <button className='bg-black text-white text-sm uppercase rounded-3xl mx-12 p-2'>Legendary Ranks</button>
-        <div className='uppercase text-black gap-8 px-9 tracking-widest font-bold flex '>
+        <div className='uppercase text-black gap-8 px-9git rebase --continue tracking-widest font-bold flex '>
           <h1 className=''>Warriors</h1> 
           <h1 className=''>Philosophies</h1>
           <h1 className=''>Alliances</h1>
